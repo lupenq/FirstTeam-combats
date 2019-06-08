093849ce8876d13a0ba26de9031d41c691ceb057
Greeds <monstromag@mail.ru>
2019-06-08 17:44:41 +0300

    Check on zero

--- js/register.js
+++ js/register.js
@@ -8,10 +8,12 @@ formReg.addEventListener('submit', function(e) {
   var xhr = new XMLHttpRequest();
   var body = 'username=' + nameField.value +
     '&password=' + passwordField.value;
-  xhr.open("POST", '/register', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(body);
-  xhr.onload = function() {
-    document.location.href = 'http://localhost:3333/auth.html';
-  };
+  if(!(nameField.value)==="" && !(passwordField.value)===""){
+    xhr.open("POST", '/register', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(body);
+    xhr.onload = function () {
+        document.location.href = 'http://localhost:3333/auth.html';
+    };
+  }
 });
